ac756a157c1a0f66eb91f52eb76a0c1fea327258
Lord Chuffnel <fs-j@live.com>
2019-01-25 15:38:21 -0800

    about us info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About us</h1>
+    <p>Radio of the Master's Voice is a one-of-a-kind station that is dedicated to the transmission of true worship and Spirit-inspired messages that will foster real spiritual growth in listeners. We are completely non-profit, and do not air advertisements--our station is completely faith-based and listener-supported.
+    </p>
   </div>
 </template>
