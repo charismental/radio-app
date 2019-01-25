e13fd1ac75b2c90b48464eb24f1e2c2000c54e4c
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2019-01-24 22:47:29 -0800

    Added state, mutations and actions to vuex, connected stream

---
+++ src/components/Timer.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="timer"><span>{{ time || "00:00" }}</span></div>
+</template>
+
+<script>
+export default {
+  name: 'Timer',
+  computed: {
+    currentTime: function () {
+      return this.$store.state.currentTime
+    },
+    time: function () {
+      return this.minutes + ':' + this.seconds
+    },
+    minutes: function () {
+      var min = Math.floor(this.currentTime / 60)
+      return min >= 10 ? min : '0' + min
+    },
+    seconds: function () {
+      var sec = this.currentTime - (this.minutes * 60)
+      return sec >= 10 ? sec : '0' + sec
+    }
+  }
+}
+</script>
+
+<style>
+/* .timer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+} */
+.timer span {
+  font-size: 66px;
+  transform: scale(.7, 1);
+  display: inline-block;
+  margin-left: -10px;
+}
+</style>
