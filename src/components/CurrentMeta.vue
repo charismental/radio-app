158083e806a1890a905503c155b91488f6bc4df7
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2019-01-29 09:03:45 -0800

    Current Meta as component

---
+++ src/components/CurrentMeta.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="current-meta" :class="[expandedPlayer ? 'expanded-meta' : 'minimized-meta']">
+      <span class="title">{{ song.title }}</span>
+      <span class="artist">{{ song.artist }}</span>
+      <span class="album">{{ song.album }}</span>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Current-Meta',
+  props: [ 'song' ],
+  computed: {
+    ...mapState([
+      'expandedPlayer'
+    ])
+  }
+}
+</script>
+
+<style>
+.minimized-meta {
+  padding-top: 5px;
+  padding-left: 0;
+}
+.expanded-meta {
+  padding-left: 15px;
+  padding-top: 10px;
+}
+.title {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  font-weight: 300;
+  font-size: 18px;
+  display: block;
+}
+.artist {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  font-weight: 100;
+  font-size: 14px;
+  display: block;
+}
+.album {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  font-weight: 100;
+  font-size: 14px;
+  display: block;
+}
+</style>
