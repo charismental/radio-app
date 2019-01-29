a191fbb5f431fbb1d5e656c666593cb0e6c92247
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2019-01-29 14:45:01 -0800

    Styled about us view slightly

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,52 @@
 <template>
   <div class="about">
-    <h1>About us</h1>
-    <p>Radio of the Master's Voice is a one-of-a-kind station that is dedicated to the transmission of true worship and Spirit-inspired messages that will foster real spiritual growth in listeners. We are completely non-profit, and do not air advertisements--our station is completely faith-based and listener-supported.
-    </p>
+    <div class="about-header">
+      <div class="divider"><hr></div>
+      <span class="about-title">About Us</span>
+      <div class="divider"><hr></div>
+      <div class="about-body">
+        <p>Radio of the Master's Voice is a one-of-a-kind station that is dedicated to the transmission of true worship and Spirit-inspired messages that will foster real spiritual growth in listeners. We are completely non-profit, and do not air advertisements--our station is completely faith-based and listener-supported.</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+.about {
+  position: absolute;
+  display: flex;
+  text-align: center;
+  top: 181px;
+  width: 360px;
+}
+.about-header {
+  width: 360px;
+  position: fixed;
+}
+.about-title {
+  font-weight: 500;
+  font-size: 32px;
+}
+.about hr  {
+  border: 0;
+  height: 1px;
+  background-image: linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.75), rgba(255, 255, 255, 0));
+}
+.about-body {
+  overflow: auto;
+  max-height: 58.5vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: white;
+  padding-left: 10px;
+}
+@media only screen and (max-width: 359px) and (min-width: 320px) {
+  .about-header {
+    width: 320px;
+  }
+  .about-body {
+    width: 290px;
+  }
+}
+</style>
