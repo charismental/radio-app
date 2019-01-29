8d350b436fadb81acc3b439b4cd4566fb742cc56
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2019-01-29 09:47:19 -0800

    Started building out recent history

---
+++ src/views/History.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="history">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import RadioPlayer from '@/components/RadioPlayer.vue'
+
+export default {
+  name: 'history'
+}
+</script>
+
+<style>
+/* .history {
+  grid-area: k;
+} */
+</style>
