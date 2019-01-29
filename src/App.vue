1df9435569124ee3cf1e29d47ab00f0dcd1a219c
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2019-01-29 09:45:43 -0800

    Building out router-view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,44 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <router-view/>
+    <radio-player />
+    <router-view v-if="!expandedPlayer" />
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import RadioPlayer from '@/components/RadioPlayer.vue'
+import { mapState } from 'vuex'
 
+export default {
+  name: 'home',
+  components: {
+    RadioPlayer
+  },
+  computed: {
+    ...mapState([
+      'expandedPlayer'
+    ])
+  }
+}
+</script>
 <style>
+#app {
+  background: url(./assets/background.png) no-repeat;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  color: #FFF;
+  display: grid;
+  height: 100vh;
+}
+@media screen and (min-width: 768px) {
+    #app {
+    border-radius: 10px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
+    margin: 0 auto;
+    width: 360px;
+    height: 640px;
+  }
+}
 </style>
