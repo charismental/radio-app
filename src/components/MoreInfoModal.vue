3bfa86e2a646986566168fd095fe0480a201ea57
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2019-01-31 17:58:07 -0800

    Modal now displays dynamic song content

--- src/components/MoreInfoModal.vue
+++ src/components/MoreInfoModal.vue
@@ -2,8 +2,14 @@
     <transition-group name="fade">
       <div class="more-info-modal" v-if="moreInfoModalDisplay" key="1">
         <div class="modal-inner">
-          <span class="more-info-header">More Info</span>
+          <span class="more-info-header">{{ modalObject.title }}</span>
           <div class="divider"><hr></div>
+          <div class="modal-body">
+            <span class="more-info-artist"><i class="material-icons">whatshot</i> Artist: {{ modalObject.artist }}</span>
+            <span class="more-info-album"><i class="material-icons">album</i> Album: {{ modalObject.album }}</span>
+            <span class="more-info-duration"><i class="material-icons">av_timer</i>Duration: {{ modalObject.minsec }}</span>
+            <span class="more-info-buycd" v-if="modalObject.buycd"><a :href="modalObject.buycd" target="_blank"><i class="material-icons">exit_to_app</i>Buy CD</a></span>
+          </div>
         </div>
       </div>
       <div class="modal-overlay" v-show="moreInfoModalDisplay" @click="toggleMoreInfoModal" key="2"></div>
@@ -17,7 +23,8 @@ export default {
   name: 'More-Info-Modal',
   computed: {
     ...mapState([
-      'moreInfoModalDisplay'
+      'moreInfoModalDisplay',
+      'modalObject'
     ])
   },
   methods: {
@@ -52,15 +59,26 @@ export default {
 }
 .more-info-modal {
   position: absolute;
-  display: inline-flex;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 30vh;
+  top: 50%;
+  margin-top: -150px;
+  min-height: 300px;
   z-index: 1010;
   background: white;
   color: #696969;
   text-align: center;
+  padding-top: 5px;
+}
+.modal-body {
+  text-align: left;
+  padding-left: 15px;
+    padding-top: 10px;
+}
+.modal-body a {
+  text-decoration: none;
+  color: inherit;
+}
+.modal-body span {
+  display: block;
 }
 .modal-overlay {
   z-index: 1000;
@@ -73,7 +91,7 @@ export default {
 }
 @media screen and (min-width: 768px) {
   .more-info-modal {
-    border-radius: 0 0 10px 10px;
+    border-radius: 10px 10px;
     box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
     width: 360px;
   }
